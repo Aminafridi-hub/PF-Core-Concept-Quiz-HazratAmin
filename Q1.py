3c4fb6f81113b1b8b052d81bf69248060f3b31f5
Aminafridi-hub <aminafridi1021@gmail.com>
2025-12-07 16:54:49 +0500

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,30 @@
+
+# Dear Sir i am using Python in this Quiz.
+
+"""
+Write a program for an Internet café billing N customers. Read minutes used for each. 
+Charge 4 per minute. If minutes > 120 apply 8% discount for that customer. 
+Print bill per customer and total. 
+Use variables, IO, loops, if/else. Push to GitHub and paste repository URL.
+"""
+
+
+n = int(input("Enter Number of Customer: "))
+totat_revenue = 0
+
+for i in range(n):
+    min_taken_Each_Customer = int(input(f"Enter time taken in minute of customer # {i + 1}: "))
+    bill_per_customer = min_taken_Each_Customer * 4
+
+    # Applying Discount
+    if min_taken_Each_Customer > 120:
+        bill_per_customer = (min_taken_Each_Customer * 4) * (1 - 0.08)
+        print(f"Bill of Customer # {i + 1} is PKR {bill_per_customer:.2f}/- Also 8% is off for you. ")
+    else:
+        print(f"Bill of Customer # {i + 1} is PKR {bill_per_customer:.2f}/-")
+
+# Print Total Bill of all Customer
+# for i in range(n):
+    totat_revenue += bill_per_customer
+    
+print(f"Total Revenue of all customer is PKR {totat_revenue:.2f} ")
